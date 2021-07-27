ecf3f36a70c2584e4458609527acd49c5361c561
Δlain <ing.alainvargas@gmail.com>
2021-07-27 04:21:07 +0300

    Add assignment

---
+++ codigo/longitud.asm
@@ -0,0 +1,36 @@
+section .data
+
+   result db 'longitud de tu nombre:'
+   resultLen equ $-result
+
+   myNameLen  equ $-myName
+
+section .bss
+
+   len2char resb 1
+
+section .text
+
+   global _start
+
+_start:
+   mov ecx,result
+   mov edx,resultLen
+   mov eax,4
+   mov ebx,1
+   int 80h
+
+   mov edx,myNameLen
+   add edx,48
+   mov [len2char],edx
+
+   mov ecx,len2char
+   mov edx,1
+   mov eax,4
+   mov ebx,1
+   int 80h
+
+   ; Termina programa 
+   mov eax,1
+   mov ebx,0
+   int 80h
