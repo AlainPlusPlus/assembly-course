0b552a2cf10832ad2c1a851ae0f1f08f7cb9bbf9
Δlain <ing.alainvargas@gmail.com>
2022-04-18 22:10:00 +0300

    Add comments

--- code/x86/MASM/hola.asm
+++ code/x86/MASM/hola.asm
@@ -4,19 +4,22 @@
 ; Programa básico de "hello, world!" en español.
 ;
 
+;Define espacios de memoria en sección de datos
 .data
-   hello db '¡Hola, mundo!'
-   helloLen  equ $- hello
+   hello db '¡Hola, mundo!'   ; cadena a imprimir
+   helloLen  equ $- hello     ; longitud de cadena 
 
+;Instrucciones van en la sección de código
 .code
 _start:
-   mov eax, 4
-   mov ebx, 1
-   lea ecx, hello 		;carga dirección efectiva (Load Effective Address)
-   mov edx, helloLen
-   int      80h
+   mov eax, 4           ; 'write' llamada al sistema = 4
+   mov ebx, 1           ; descriptor de archivo 1 = STDOUT
+   lea ecx, hello 		; carga dirección efectiva (Load Effective Address) de mensaje a escribir
+   mov edx, helloLen    ; longitud del mensaje a escribir
+   int      80h         ; llamada al kernel (interrupción)
 
-   mov eax, 1
-   mov ebx, 0
-   int      80h
+   ;Termina programa 
+   mov eax, 1           ; 'exit' llamada al sistema = 1
+   mov ebx, 0           ; finaliza con código de error = 0
+   int      80h         ; llamada al kernel (interrupción)
 end _start
